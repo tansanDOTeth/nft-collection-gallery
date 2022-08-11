09272a3edebfa053eb3cf621a5e3a328b250f18a
kminer <kennerminer@gmail.com>
2022-08-10 18:59:25 -0700

    Remove pagination and add virtualized view for to add "infinite" scrollability without degrading performance for large collections.

--- src/components/CollectionGallery/CollectionGallery.js
+++ src/components/CollectionGallery/CollectionGallery.js
@@ -1,16 +1,9 @@
-import {
-  AppBar,
-  Box,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Pagination,
-  Paper,
-  Select,
-  Toolbar,
-} from '@mui/material';
-import { LeftColumn, RightColumn, ScrollableBox } from 'components/Layout';
-import React, { useState } from 'react';
+import { Paper } from '@mui/material';
+import { LeftColumn, RightColumn } from 'components/Layout';
+import React from 'react';
+import { FixedSizeGrid } from 'react-window';
+import AutoSizer from 'react-virtualized-auto-sizer';
+
 import { TraitFilterContextConsumer, TraitFilterContextProvider } from 'contexts/TraitFilterContext';
 
 import { Content } from 'components/Layout';
@@ -19,28 +12,12 @@ import GalleryItem from './GalleryItem';
 import TraitFilters from 'components/TraitFilters';
 
 const CollectionGallery = ({ tokens }) => {
-
-  const [perPage, setPerPage] = useState(15);
-  const [page, setPage] = useState(1);
-  const onPageChange = (event, newPage) => setPage(newPage)
-
-  const currentPageTokens = (filteredTokens) => {
-    const startIndex = (page - 1) * perPage;
-    const endINdex = (page * perPage);
-    return filteredTokens.slice(startIndex, endINdex)
-  }
-
-  const updatePerPage = (event) => {
-    setPerPage(event.target.value);
-    setPage(1)
-  };
-
   return (
     <Paper
       elevation={3}
       sx={{ position: 'fixed', top: 0, bottom: 0, left: 0, right: 0, zIndex: 999, p: 1 }}
     >
-      <TraitFilterContextProvider tokens={tokens} onFilterChange={() => setPage(1)}>
+      <TraitFilterContextProvider tokens={tokens}>
         <Content sx={{ flexDirection: 'row' }}>
           <LeftColumn>
             <LeftColumn.ColumnBox>
@@ -61,7 +38,6 @@ const CollectionGallery = ({ tokens }) => {
             </LeftColumn.ColumnBox>
           </LeftColumn>
           <RightColumn>
-            <ScrollableBox sx={{ display: 'flex', flexDirection: 'column', mb: 5 }}>
               <TraitFilterContextConsumer>
                 {({ selectedFilters, removeFilter }) =>
                   <CurrentFilters filters={selectedFilters} onDelete={removeFilter} />
@@ -69,42 +45,44 @@ const CollectionGallery = ({ tokens }) => {
               </TraitFilterContextConsumer>
               <TraitFilterContextConsumer>
                 {({ filteredTokens }) =>
-                  <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-                    {currentPageTokens(filteredTokens).map((token) => <GalleryItem key={token.name} token={token} />)}
-                  </Box>
+                    <AutoSizer>
+                      {({ height, width }) => {
+                        const ROW_GUTTER = 20;
+                        const COLUMN_GUTTER = 20;
+                        const CARD_WIDTH = 250;
+                        const CARD_HEIGHT = 290;
+
+                        const columnCount = Math.floor(width / (CARD_WIDTH + COLUMN_GUTTER));
+                        const rowCount = Math.ceil(filteredTokens.length / columnCount);
+
+                        return (
+                          <FixedSizeGrid
+                            columnCount={columnCount}
+                            columnWidth={width / columnCount}
+                            height={height}
+                            rowCount={rowCount}
+                            rowHeight={CARD_HEIGHT + ROW_GUTTER}
+                            width={width}
+                          >
+                            {({ columnIndex, rowIndex, style }) => {
+                              const token = filteredTokens[rowIndex * columnCount + columnIndex];
+
+                              if (!token) {
+                                return null;
+                              }
+                              
+                              return (
+                                <div style={style}>
+                                  <GalleryItem token={token} />
+                                </div>
+                              )
+                            }}
+                          </FixedSizeGrid>
+                        )
+                      }}
+                    </AutoSizer>
                 }
               </TraitFilterContextConsumer>
-            </ScrollableBox>
-            <AppBar position='sticky' sx={{ bottom: 0, height: 72 }}>
-              <TraitFilterContextConsumer>
-                {({ filteredTokens }) =>
-                  <Toolbar>
-                    <Pagination
-                      sx={{ flexGrow: 1 }}
-                      count={Math.ceil(filteredTokens.length / perPage)}
-                      page={page}
-                      onChange={onPageChange}
-                      siblingCount={5}
-                      boundaryCount={5}
-                      shape="rounded" />
-                    <FormControl sx={{ m: 1, minWidth: 80 }}>
-                      <InputLabel>Per Page</InputLabel>
-                      <Select
-                        value={perPage}
-                        onChange={updatePerPage}
-                        autoWidth
-                        label="Per Page"
-                      >
-                        <MenuItem value={15}>15</MenuItem>
-                        <MenuItem value={25}>25</MenuItem>
-                        <MenuItem value={50}>50</MenuItem>
-                        <MenuItem value={100}>100</MenuItem>
-                      </Select>
-                    </FormControl>
-                  </Toolbar>
-                }
-              </TraitFilterContextConsumer>
-            </AppBar>
           </RightColumn>
         </Content>
       </TraitFilterContextProvider>
