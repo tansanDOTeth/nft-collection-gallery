a581f45d59a8ef4fa871fafd97b5b24deca8b8ea
tansan.eth <92072158+tansanDOTeth@users.noreply.github.com>
2022-08-24 00:46:09 +0900

    feat(defaultcurrentfilterslayout): convert layout to basic html and css
    Remove mui component

--- src/components/DefaultCurrentFiltersLayout/DefaultCurrentFiltersLayout.js
+++ src/components/DefaultCurrentFiltersLayout/DefaultCurrentFiltersLayout.js
@@ -1,12 +1,43 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
-import CurrentFilters from 'components/CurrentFilters';
+const styles = {
+  wrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    listStyle: 'none',
+    padding: '0.5rem',
+    margin: 0,
+  },
+  filter: {
+    border: '1px solid grey',
+    borderRadius: 2,
+    padding: '0.5rem',
+    marginRight: '0.5rem',
+    cursor: 'pointer'
+  }
+}
+
+const SelectedFilter = ({ name, onClick }) =>
+  <li
+    style={styles.filter}
+    onClick={() => onClick(name)}>
+    {name}
+  </li>
 
 const DefaultCurrentFiltersLayout = ({ TraitFilterContextConsumer }) =>
-  <Fragment>
+  <ul style={styles.wrapper}>
     <TraitFilterContextConsumer>
-      {({ selectedFilters, removeFilter }) => <CurrentFilters filters={selectedFilters} onDelete={removeFilter} />}
+      {({ selectedFilters, removeFilter }) =>
+        Object.entries(selectedFilters)
+          .filter(([name, isChecked]) => isChecked)
+          .map(([name]) =>
+            <SelectedFilter
+              key={name}
+              name={name}
+              onClick={removeFilter} />
+          )
+      }
     </TraitFilterContextConsumer>
-  </Fragment>;
+  </ul>;
 
 export default DefaultCurrentFiltersLayout;
