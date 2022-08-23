f89be96f84a41f8258001cdea184a9f2c0926974
tansan.eth <92072158+tansanDOTeth@users.noreply.github.com>
2022-08-24 01:31:09 +0900

    feat(defaultfilterslayout): convert layout to basic html and css
    Remove mui components

--- src/components/DefaultFiltersLayout/DefaultFiltersLayout.js
+++ src/components/DefaultFiltersLayout/DefaultFiltersLayout.js
@@ -1,25 +1,83 @@
-import { LeftColumn } from 'components/Layout';
 import React from 'react';
-import TraitFilters from 'components/TraitFilters';
+
+const TraitFilter = ({ traitName, variationNames, getCount, onChange, isChecked }) => {
+  const getKeyName = (traitName, variationName) => `${traitName}:${variationName}`
+
+  const styles = {
+    traitHeader: {
+      paddingTop: '1rem',
+      paddingBottom: '1rem',
+      textAlign: 'left'
+    },
+    variationNames: {
+      textAlign: 'left'
+    },
+    variationName: {
+      display: 'flex',
+      flexGrow: 1,
+      justifyContent: 'space-between'
+    }
+  }
+
+  return (
+    <div>
+      <div style={styles.traitHeader}>
+        {traitName}
+      </div>
+      <div style={styles.variationNames}>
+        {(variationNames || [])
+          .sort((variationNameA, variationNameB) =>
+            getCount(getKeyName(traitName, variationNameB)) - getCount(getKeyName(traitName, variationNameA)))
+          .map((variationName) => {
+            const keyName = getKeyName(traitName, variationName);
+            return (
+              <label
+                style={styles.variationName}
+                name={keyName}>
+                <div>
+                  <input
+                    name={keyName}
+                    type="checkbox"
+                    checked={isChecked(keyName) || false}
+                    onChange={onChange}
+                    style={{ marginRight: '0.5rem' }}
+                  />
+                  <span>{variationName}</span>
+                </div>
+                <span>{getCount(keyName)}</span>
+              </label>
+            )
+          })}
+      </div>
+    </div>
+  )
+}
+
+const TraitFilters = ({ variationNamesByTraitName, ...otherProps }) =>
+  Object.entries(variationNamesByTraitName)
+    .sort((a, b) => a[0].localeCompare(b[0]))
+    .map(([traitName, variationNames]) =>
+      <TraitFilter
+        key={traitName}
+        traitName={traitName}
+        variationNames={variationNames}
+        {...otherProps} />
+    )
 
 const DefaultFiltersLayout = ({ TraitFilterContextConsumer }) =>
-  <LeftColumn>
-    <LeftColumn.ColumnBox>
-      <TraitFilterContextConsumer>
-        {({ variationNamesByTraitName, tokensByVariationName, selectedFilters, setSelectedFilters }) =>
-          <TraitFilters
-            variationNamesByTraitName={variationNamesByTraitName}
-            getCount={(key) => tokensByVariationName[key].length}
-            onChange={({ target: { name, checked } }) => {
-              setSelectedFilters({
-                ...selectedFilters,
-                [name]: checked
-              });
-            }}
-            isChecked={(key) => selectedFilters[key]} />
-        }
-      </TraitFilterContextConsumer>
-    </LeftColumn.ColumnBox>
-  </LeftColumn>;
+  <TraitFilterContextConsumer>
+    {({ variationNamesByTraitName, tokensByVariationName, selectedFilters, setSelectedFilters }) =>
+      <TraitFilters
+        variationNamesByTraitName={variationNamesByTraitName}
+        getCount={(key) => tokensByVariationName[key].length}
+        onChange={({ target: { name, checked } }) => {
+          setSelectedFilters({
+            ...selectedFilters,
+            [name]: checked
+          });
+        }}
+        isChecked={(key) => selectedFilters[key]} />
+    }
+  </TraitFilterContextConsumer>
 
 export default DefaultFiltersLayout;
