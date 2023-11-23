3bf6e7c1f632f420ca6b775056eb066a9260ca5f
tansan.eth <92072158+tansanDOTeth@users.noreply.github.com>
2023-11-23 22:26:34 +0900

    style: linting

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,35 @@
-import autoprefixer from 'autoprefixer';
-import babel from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import postcss from 'rollup-plugin-postcss';
-import { uglify } from 'rollup-plugin-uglify';
+import autoprefixer from "autoprefixer";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import postcss from "rollup-plugin-postcss";
+import { uglify } from "rollup-plugin-uglify";
 
 const config = {
-  input: 'src/components/CollectionGallery/index.js',
-  external: [
-    'react'
-  ],
+  input: "src/components/CollectionGallery/index.js",
+  external: ["react"],
   output: {
     format: "es",
-    name: 'CollectionGallery',
+    name: "CollectionGallery",
     globals: {
-      react: "React"
-    }
+      react: "React",
+    },
   },
   plugins: [
     postcss({
       extract: false,
-      plugins: [
-        autoprefixer,
-      ],
+      plugins: [autoprefixer],
     }),
     nodeResolve({
-      moduleDirectories: [
-        'src',
-        'node_modules'
-      ]
+      moduleDirectories: ["src", "node_modules"],
     }),
     babel({
-      babelHelpers: 'bundled',
-      exclude: /node_modules/
+      babelHelpers: "bundled",
+      exclude: /node_modules/,
     }),
     commonjs(),
-    uglify()
+    uglify(),
   ],
 };
 
-export default config
+export default config;
